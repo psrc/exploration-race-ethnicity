3ef0a2e5032f59892e2d3dcd111f31491fa50bc7
Mary Richards <mrichards@psrc.org>
2025-05-16 20:36:39 -0700

    creating hh size counts

---
+++ household-size/03-hh-size-race-non-total.R
@@ -0,0 +1,53 @@
+# This script will produce a rds that compiles non-total median income by geography, race, and table detail types
+
+library(tidyverse)
+library(magrittr)
+
+# retrieve data
+unloadNamespace("psrccensus")
+source("household-size/01-hh-size-race-get-data.R")
+
+race_vars <- c("ARACE", "PRACE", "HRACE")
+table_types <- c("detail", "dichot", "single")
+
+# compile non-aggregated totals (i.e not Multirace, Multiple Races, POC) ----
+
+main_df <- NULL
+for(ttype in table_types) {
+  
+  # find table object
+  dl <- get(ls(pattern = ttype))
+  
+  for(var in race_vars) {
+    count_reg <- psrc_pums_count(dl, 
+                                 group_vars=c(var, "hhsz_binary"), 
+                                 incl_na=FALSE, 
+                                 rr=TRUE) %>%
+      filter(hhsz_binary == "single-person") #%>%
+      # filter(hhsz_binary == "multi-person") #or multi-person
+    
+    count_cnty <- psrc_pums_count(dl, 
+                                  group_vars=c("COUNTY", var, "hhsz_binary"), 
+                                  incl_na=FALSE, 
+                                  rr=TRUE) %>%
+      filter(hhsz_binary == "single-person") %>%
+      # filter(hhsz_binary == "multi-person") |> #or multi-person
+      filter(COUNTY != "Region")
+    
+    # rename var to generic colnames to assemble and add new column to identify type of raw table
+    rs <- bind_rows(count_reg, count_cnty) |>
+      mutate(race_type = var,
+             table_type = ttype) |>
+      mutate(COUNTY = factor(COUNTY, levels = c("Region", "King", "Kitsap", "Pierce", "Snohomish"))) |>
+      rename(race = var) |> 
+      arrange(COUNTY)
+    
+    # bind to main table
+    ifelse(is.null(main_df), main_df <- rs, main_df <- bind_rows(main_df, rs))
+  }
+}
+
+saveRDS(main_df, "household-size/data/non-total-counts-df-singleperson.rds")
+# saveRDS(main_df, "household-size/data/non-total-counts-df-multiperson.rds")
+# readRDS("household-size/data/non-total-counts-df-singleperson.rds")
+
---
+++ household-size/04-hh-size-race-compile-format.R
@@ -0,0 +1,64 @@
+# This script combines the two RDS files and separates the data frames by geography, median values, and reliability into 
+# an excel workbook
+
+library(tidyverse)
+library(openxlsx)
+
+race_vars <- c("ARACE", "PRACE", "HRACE")
+table_types <- c("detail", "dichot", "single")
+
+file_names <- c("non-total-counts-df-singleperson.rds", "total-counts-df-singleperson.rds")
+# file_names <- c("non-total-counts-df-multiperson.rds","total-counts-df-multiperson.rds")
+
+# compile into one df ----
+
+df_bind <- map(file_names, ~readRDS(file.path("household-size/data/", .x))) |> 
+  bind_rows() |> 
+  rename_with(toupper, c(race, table_type))
+
+dfs_count <- df_bind |> 
+  select(!c(reliability, ends_with("moe")))
+
+dfs_rel <- df_bind |> 
+  select(!starts_with("H"))
+
+# separate into different dataframes ----
+
+geographies <- unique(df_bind$COUNTY)
+all_dfs <- list()
+
+# Loop through the list of dataframes and add them as sheets ----
+
+for (ttype in table_types) {
+  for(g in geographies) {
+    id_cols <- c("DATA_YEAR", "COUNTY", "RACE", "TABLE_TYPE")
+    
+    df_count <- dfs_count |>
+      filter(TABLE_TYPE == ttype & COUNTY == g) |>
+      pivot_wider(id_cols = id_cols,
+                  names_from = "race_type",
+                  names_glue = "{race_type}_{.value}",
+                  values_from = "count")
+    
+    df_share <- dfs_count |>
+      filter(TABLE_TYPE == ttype & COUNTY == g)|>
+      pivot_wider(id_cols = id_cols,
+                  names_from = "race_type",
+                  names_glue = "{race_type}_{.value}",
+                  values_from = "share")
+    
+    df_rel <- dfs_rel |>
+      filter(TABLE_TYPE == ttype & COUNTY == g)|>
+      pivot_wider(id_cols = id_cols,
+                  names_from = "race_type",
+                  names_glue = "{race_type}_{.value}",
+                  values_from = "reliability")
+    
+    all_dfs[[paste(g, ttype, "count", sep = "_")]] <- df_count
+    all_dfs[[paste(g, ttype, "share", sep = "_")]] <- df_share
+    all_dfs[[paste(g, ttype, "reliability", sep = "_")]] <- df_rel
+  }
+}
+
+write.xlsx(all_dfs, "household-size/data/household-size-by-re-singleperson.xlsx")
+# write.xlsx(all_dfs, "household-size/data/household-size-by-re-multiperson.xlsx")
