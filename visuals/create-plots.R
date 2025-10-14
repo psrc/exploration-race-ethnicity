13cc0af5f448f25ac44489d181e8eecef09e68b4
cyyllam <cyyllam@gmail.com>
2025-10-14 11:03:46 -0700

    rename files

--- visuals/create-plots.R
+++ visuals/create-plots.R
@@ -5,56 +5,99 @@ library(showtext)
 library(psrcplot)
 library(here)
 
-filenames <- c("household-count", "ownership_rate", "renter-cost-burden", "median-income")
+source(here::here('visuals/function-plot.R'))
+# indicators <- c("household-count", "ownership_rate", "renter-cost-burden", "median-income")
+create_plots <- function(indicator, vars_options = c(1, 2, 3), ind_value = c("share", "count", "median")) {
+  
+  if(indicator == "median-income" & ind_value != "median") {
+    message("The indicator output value for 'median-income' must be 'median'")
+    return(NULL) # Exit the function and return NULL
+  }
+  
+  vars <- c("-by-re.xlsx", "-by-re-hhsize.xlsx", "-by-re-hhsize-tenure.xlsx")
+  
+  e <- data.frame(first = c("detail", "dichot"), second = c("mp", "sp"), third = c("own", "rent"))
+  
+  df_e <- expand.grid(e) |> 
+    mutate(vo2 = paste(first, second, sep = "_")) |> 
+    mutate(vo3 = paste(first, second, third, sep = "_"))
+  
+  all_plots <- list()
+  all_tables <- list()
+  
+  datafile <- paste0(indicator, vars[[vars_options]])
+  
+  if(vars_options == 1) {
+    tab_names <- unique(df_e$first)
+  } else if(vars_options == 2) {
+    tab_names <- unique(df_e$vo2)
+  } else if(vars_options == 3) {
+    tab_names <- unique(df_e$vo3)
+  }
+  
+  for(t in tab_names) {
+    df <- read.xlsx(here::here("visuals", datafile), sheet = t)
+    
+    r <- df |> 
+      filter(COUNTY == "Region") |> 
+      select(RACE, ends_with(c("share", "count"))) 
+    
+    r <- df |> 
+      filter(COUNTY == "Region") |> 
+      select(RACE, ends_with(ind_value), ends_with(paste0(ind_value, "_moe"))) 
+    
+    races_ord <- r$RACE
+    
+    df_long <- r |>
+      pivot_longer(cols = -RACE,
+                   names_to = "variable",
+                   values_to = "value"
+      ) |>
+      mutate(
+        # Create median/moe indicator
+        type = if_else(str_detect(variable, "_moe$"), "moe", ind_value),
 
-# median-inc tier 3
-e <- data.frame(first = c("detail", "dichot"), second = c("mp", "sp"), third = c("own", "rent"))
-df_e <- expand.grid(e) |> 
-  mutate(tab_name = paste(first, second, third, sep = "_")) |> 
-  mutate(second_text = case_when(second == "mp" ~ "Multi-Person",
-                                 second == "sp" ~ "Single-Person")) |> 
-  mutate(plot_name = str_to_title(paste(first, second_text, third)))
+        # Clean variable to just the description
+        description = str_remove(variable, "_moe$"),
+        description = str_trim(str_replace(description, paste0("_", ind_value), "")),
+        description = str_replace(description, "_?HINCP_?", "")
+      ) |>
+      select(RACE, description, type, value) |>
+      pivot_wider(names_from = type,
+                  values_from = value
+      ) |>
+      mutate(upper = !!sym(ind_value) + moe,
+             lower = !!sym(ind_value) - moe) |>
+      mutate(description = factor(description, levels = c("PRACE", "ARACE", "HRACE")),
+             RACE = factor(RACE, levels = races_ord)) |>
+      arrange(RACE, description)
+    
+    all_tables[[t]] <- df_long
 
-all_plots <- list()
+    plot_name <- str_replace_all(vars[vars_options], "-", " ") |> 
+      str_replace_all(".xlsx", "") |> 
+      str_replace_all("\\sre", " Race and Ethnicity") |> 
+      str_replace_all("hhsize", "Household Size")
+    
+    plot_name <- str_replace_all(plot_name, "Ethnicity ", "Ethnicity, " ) |> 
+      str_replace_all(" tenure", ", and Tenure")
 
-for(t in df_e$tab_name) {
+    subtitle_name <-  str_replace_all(t, "_", " ") |> 
+      str_to_title() |> 
+      str_replace_all("Mp", "Multi-Person") |>   
+      str_replace_all("Sp", "Single-Person")
+    
+    ind <- str_replace_all(indicator, "-", " ") |> 
+      str_to_title()
+    
+    sub <- str_squish(paste0(subtitle_name, plot_name))
+    all_plots[[t]] <- create_bar_chart(df = df_long, title = ind, subtitle = sub)
+  }
   
-  chart_name <- df_e |> filter(tab_name == t) |> pull(plot_name)
-  
-  df <- read.xlsx(datafile,
-                  sheet = t)
-  
-  r <- df |> 
-    filter(COUNTY == "Region") |> 
-    select(RACE, ends_with("median"), ends_with("moe")) 
-  
-  races_ord <- r$RACE
-  
-  df_long <- r |> 
-    pivot_longer(cols = -RACE,
-                 names_to = "variable",
-                 values_to = "value"
-    ) |> 
-    mutate(
-      # Create median/moe indicator
-      type = if_else(str_detect(variable, "_moe$"), "moe", "median"),
-      
-      # Clean variable to just the description
-      description = str_remove(variable, "_moe$"),
-      description = str_trim(str_replace(description, "_HINCP_median", ""))
-    ) |> 
-    select(RACE, description, type, value) |> 
-    pivot_wider(names_from = type,
-                values_from = value
-    ) |> 
-    mutate(upper = median + moe,
-           lower = median - moe) |> 
-    mutate(description = factor(description, levels = c("PRACE", "ARACE", "HRACE")),
-           RACE = factor(RACE, levels = races_ord)) |> 
-    arrange(RACE, description)
-  
-  p <- create_bar_chart(df = df_long, title = chart_name)
-  all_plots[[t]] <- p
+ return(list(tables = all_tables, plots = all_plots))
 }
 
-all_plots
+
+# test <- create_plots("household-count", 1, "count")
+# test2 <- create_plots("household-count", 2, "share")
+# test3 <- create_plots("median-income", 3, "median")
