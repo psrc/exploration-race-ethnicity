ec3a0f5c8c3d72939e54948445301c2fa437f20b
Mary Richards <mrichards@psrc.org>
2025-12-05 20:32:45 -0800

    something is wrong with the aggregations

--- household-size/02-hh-size-race-total.R
+++ household-size/02-hh-size-race-total.R
@@ -12,39 +12,39 @@ table_types <- c("detail", "dichot", "single")
 
 # functions ----
 # aggregating summary total rows, differentiating between Harvard and PSRC methods of grouping r/e categories
+cat_multirace_psrc <- function(df, race_col, x) {
+  newcol <- paste0(x, "cat_multirace")
+  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multirace*|^MNAW*|^Two.*"), "Multirace PSRC", "Not Multirace PSRC"))
+}
+
+cat_single_race_psrc <- function(df, race_col, x) {
+  newcol <- paste0(x, "cat_single")
+  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multirace*|^MNAW*|^Two.*"), "Not single race PSRC", "Single race PSRC"))
+}
+
+cat_multirace_harvard <- function(df, race_col, x) {
+  newcol <- paste0(x, "cat_multirace")
+  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multirace*|^MNAW*"), "Multirace Harvard", "Not Multirace Harvard"))
+}
+
+cat_single_race_harvard <- function(df, race_col, x) {
+  newcol <- paste0(x, "cat_single")
+  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multirace*|^MNAW*"), "Not single race Harvard", "Single race Harvard"))
+}
+
 cat_poc <- function(df, race_col, x) {
   newcol <- paste0(x, "cat_poc")
   df |> mutate({{newcol}} := ifelse(!c(.data[[race_col]] %in% c("Total", "White", NA)), "People of color", "Not People of Color"))
 }
 
-cat_multirace_psrc <- function(df, race_col, x) {
-  newcol <- paste0(x, "cat_multiple")
-  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multi.*|^Two.*"), "Multirace PSRC", "Not Multirace PSRC"))
-}
-
-cat_single_race_psrc <- function(df, race_col, x) {
-  newcol <- paste0(x, "cat_single")
-  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multi.*|^Two.*"), "Not single race PSRC", "Single race PSRC"))
-}
-
-cat_multirace_harvard <- function(df, race_col, x) {
-  newcol <- paste0(x, "cat_multi")
-  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multi.*"), "Multirace Harvard", "Not Multirace Harvard"))
-}
-
-cat_single_race_harvard <- function(df, race_col, x) {
-  newcol <- paste0(x, "cat_single")
-  df |> mutate({{newcol}} := ifelse(str_detect(.data[[race_col]], "^Multi.*"), "Not single race Harvard", "Single race Harvard"))
-}
-
 create_total_counts <- function(raw_pums) {
   
   # add labeling columns
-  dl <- reduce2(race_vars, list("p", "a", "h"), cat_poc, .init = raw_pums)
-  dl <- reduce2(race_vars, list("p", "a", "h"), cat_multirace_psrc, .init = dl)
+  dl <- reduce2(race_vars, list("p", "a", "h"), cat_multirace_psrc, .init = raw_pums)
   dl <- reduce2(race_vars, list("p", "a", "h"), cat_single_race_psrc, .init = dl)
   dl <- reduce2(race_vars, list("p", "a", "h"), cat_multirace_harvard, .init = dl)
   dl <- reduce2(race_vars, list("p", "a", "h"), cat_single_race_harvard, .init = dl)
+  dl <- reduce2(race_vars, list("p", "a", "h"), cat_poc, .init = dl)
   
   group_vars <- str_subset(colnames(dl$variables), ".*t_.*")
   
