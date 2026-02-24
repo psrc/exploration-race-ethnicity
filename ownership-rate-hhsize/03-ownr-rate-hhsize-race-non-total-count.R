e5f4e7a9c34f7fbdbb373b8ecc5be9aeea2b0b37
Mary Richards <mrichards@psrc.org>
2026-02-23 16:03:16 -0800

    adding NA rows for missing geography/race combos

--- ownership-rate-hhsize/03-ownr-rate-hhsize-race-non-total-count.R
+++ ownership-rate-hhsize/03-ownr-rate-hhsize-race-non-total-count.R
@@ -24,40 +24,73 @@ for(ttype in table_types) {
   for(var in race_vars) {
     count_reg <- psrc_pums_count(dl,
                                  group_vars = c(var,"hhsz_binary","OWN_RENT"),
-                                incl_na = FALSE,
-                                rr = TRUE) |>
+                                 incl_na = FALSE,
+                                 rr = TRUE) |>
       filter(OWN_RENT == "Owned") |>
-     filter(hhsz_binary == "single-person" |
-            hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
+      filter(hhsz_binary == "single-person" |
+               hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
     
     count_cnty <- psrc_pums_count(dl,
-                                 group_vars = c("COUNTY", var,"hhsz_binary","OWN_RENT"),
-                                 incl_na = FALSE,
-                                 rr = TRUE) |>
-      filter(COUNTY!="Region", OWN_RENT == "Owned") |>
-      filter(hhsz_binary == "single-person" |
-               hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
-    
-    # extract from tables below where: XRace == "Total"
-    count_reg2 <- psrc_pums_count(dl,
-                                 group_vars = c("hhsz_binary","OWN_RENT",var),
-                                 incl_na = FALSE,
-                                 rr = TRUE) |>
-      filter(OWN_RENT == "Owned") |>
-      filter(hhsz_binary == "single-person" |
-               hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
-    
-    count_cnty2 <- psrc_pums_count(dl,
-                                  group_vars = c("COUNTY","hhsz_binary","OWN_RENT",var),
+                                  group_vars = c("COUNTY", var,"hhsz_binary","OWN_RENT"),
                                   incl_na = FALSE,
                                   rr = TRUE) |>
       filter(COUNTY!="Region", OWN_RENT == "Owned") |>
       filter(hhsz_binary == "single-person" |
                hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
     
+    # add nulls for missing county/race values
+    # 
+    # group_levels <- c("A", "B")       # could come from a master list
+    # year_levels  <- c(2020, 2021)     # could come from a master list
+    
+    completed_count_cnty <- count_cnty %>%
+      complete(COUNTY = c("King", "Kitsap", "Pierce", "Snohomish"), #can't use unique() b/c "Region" and "Total"
+               !!sym(var) := c("American Indian or Alaskan Native", 
+                               "Asian",
+                               "Black or African American",
+                               "Hispanic or Latino",
+                               "Native Hawaiian or Pacific Islander",
+                               "Some Other Race",
+                               "Two or More Races",
+                               "White"), #can't use unique() b/c "Total"
+               hhsz_binary = unique(count_cnty$hhsz_binary),
+               DATA_YEAR = unique(count_cnty$DATA_YEAR),
+               OWN_RENT = "Owned") #can't use unique() b/c "Owned", "Rented", "Else", "Total"
+    
+    # completed_count_cnty <- count_cnty %>%
+    #   complete(COUNTY = c("King", "Kitsap", "Pierce", "Snohomish"), #can't use unique() b/c "Region" and "Total"
+    #            PRACE = c("American Indian or Alaskan Native", 
+    #                       "Asian",
+    #                       "Black or African American",
+    #                       "Hispanic or Latino",
+    #                       "Native Hawaiian or Pacific Islander",
+    #                       "Some Other Race",
+    #                       "Two or More Races",
+    #                       "White"), #can't use unique() b/c "Total"
+    #            hhsz_binary = unique(count_cnty$hhsz_binary),
+    #            DATA_YEAR = unique(count_cnty$DATA_YEAR),
+    #            OWN_RENT = "Owned") #can't use unique() b/c "Owned", "Rented", "Else", "Total"
+    
+    # extract from tables below where: XRace == "Total"
+    count_reg2 <- psrc_pums_count(dl,
+                                  group_vars = c("hhsz_binary","OWN_RENT",var),
+                                  incl_na = FALSE,
+                                  rr = TRUE) |>
+      filter(OWN_RENT == "Owned") |>
+      filter(hhsz_binary == "single-person" |
+               hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
+    
+    count_cnty2 <- psrc_pums_count(dl,
+                                   group_vars = c("COUNTY","hhsz_binary","OWN_RENT",var),
+                                   incl_na = FALSE,
+                                   rr = TRUE) |>
+      filter(COUNTY!="Region", OWN_RENT == "Owned") |>
+      filter(hhsz_binary == "single-person" |
+               hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
+    
     
     # rename var to generic colnames to assemble and add new column to identify type of raw table
-    rs <- bind_rows(count_reg, count_cnty) |>
+    rs <- bind_rows(count_reg, completed_count_cnty) |>
       mutate(race_type = var,
              table_type = ttype) |>
       mutate(COUNTY = factor(COUNTY, levels = c("Region", "King", "Kitsap", "Pierce", "Snohomish"))) |>
@@ -77,9 +110,10 @@ for(ttype in table_types) {
     
     # bind to main table
     ifelse(is.null(main_df), main_df <- rs3, main_df <- bind_rows(main_df, rs3))
+    
   }
 }
 
 saveRDS(main_df, "ownership-rate-hhsize/data/non-total-counts-df.rds")
 
-#readRDS("ownership-rate-hhsize/data/non-total-counts-df.rds")
+# readRDS("ownership-rate-hhsize/data/non-total-counts-df.rds")
