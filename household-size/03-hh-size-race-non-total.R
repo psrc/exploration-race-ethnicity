cb845bb3bb90d92435b846625c6d5c5595cd4adb
Mary Richards <mrichards@psrc.org>
2026-02-24 15:34:27 -0800

    adding NA rows for missing geography/race combos
    Didn't change outputs, but keeping hhsize code consistent

--- household-size/03-hh-size-race-non-total.R
+++ household-size/03-hh-size-race-non-total.R
@@ -26,16 +26,30 @@ for(ttype in table_types) {
                                  group_vars=c("hhsz_binary",var), 
                                  incl_na=FALSE, 
                                  rr=TRUE) #%>%
-      # filter(hhsz_binary == "single-person") #%>%
-      # filter(hhsz_binary == "multi-person") #or multi-person
+    # filter(hhsz_binary == "single-person" |
+    #          hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
     
     count_cnty <- psrc_pums_count(dl, 
                                   group_vars=c("COUNTY","hhsz_binary",var), 
                                   incl_na=FALSE, 
                                   rr=TRUE) %>%
-      # filter(hhsz_binary == "single-person") %>%
-      # filter(hhsz_binary == "multi-person") |> #or multi-person
-      filter(COUNTY != "Region")
+      filter(COUNTY != "Region") #%>% 
+      # filter(hhsz_binary == "single-person" |
+      #          hhsz_binary == "multi-person") # need to filter because added 'Total' rows with added variable
+    
+    # add nulls for missing county/race values (just in case)
+    completed_count_cnty <- count_cnty %>%
+      complete(COUNTY = c("King", "Kitsap", "Pierce", "Snohomish"), #can't use unique() b/c "Region" and "Total"
+               !!sym(var) := c("American Indian or Alaskan Native", 
+                               "Asian",
+                               "Black or African American",
+                               "Hispanic or Latino",
+                               "Native Hawaiian or Pacific Islander",
+                               "Some Other Race",
+                               "Two or More Races",
+                               "White"), #can't use unique() b/c "Total"
+               hhsz_binary = unique(count_cnty$hhsz_binary),
+               DATA_YEAR = unique(count_cnty$DATA_YEAR))
     
     # extract from tables below where: XRace == "Total" 
     count_reg2 <- psrc_pums_count(dl, 
@@ -58,18 +72,18 @@ for(ttype in table_types) {
       rename(race = var) |> 
       arrange(COUNTY)
     
-    # rs2 <- bind_rows(count_reg2, count_cnty2) |> 
+    # rs2 <- bind_rows(count_reg2, count_cnty2) |>
     #   mutate(race_type = var,
     #          table_type = ttype) |>
     #   mutate(COUNTY = factor(COUNTY, levels = c("Region", "King", "Kitsap", "Pierce", "Snohomish"))) |>
-    #   rename(race = var) |> 
-    #   arrange(COUNTY) |> 
+    #   rename(race = var) |>
+    #   arrange(COUNTY) |>
     #   filter(race == 'Total' & hhsz_binary != 'Total')
-    # 
-    # rs3 <- bind_rows(rs, rs2) |> 
+    
+    # rs3 <- bind_rows(rs, rs2) |>
     #   arrange(COUNTY, race, hhsz_binary)
     
-    rs3 <- rs |> 
+    rs3 <- rs |>
       arrange(COUNTY, race, hhsz_binary)
     
     # bind to main table
@@ -78,7 +92,4 @@ for(ttype in table_types) {
 }
 
 saveRDS(main_df, "household-size/data/non-total-counts-df.rds")
-# saveRDS(main_df, "household-size/data/non-total-counts-df-singleperson.rds")
-# saveRDS(main_df, "household-size/data/non-total-counts-df-multiperson.rds")
-
 # test2 <- readRDS("household-size/data/non-total-counts-df.rds")
